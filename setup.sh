a8206b9406a82ea8bcdd0c7478787a67306ddc90
Jakub Płoskonka <ploskonkajakub@gmail.com>
2019-10-09 22:06:41 +0100

    feat(setup): switch to npm, run codecept test on docs

--- setup.sh
+++ setup.sh
@@ -14,6 +14,17 @@ source ~/.nvm/nvm.sh
 echo "Installing node $(cat .nvmrc)"
 nvm install
 
-echo "Installing yarn packages"
-yarn install && cd codeceptjs-docs/website && yarn
+echo "Installing packages"
+npm install && (cd codeceptjs-docs && npm install)
 
+echo "Starting codeceptJS docs"
+(cd codeceptjs-docs/website && BROWSER=false npm start -- --no-watch &>/dev/null &)
+
+echo "Waiting 15 seconds for codeceptJS docs to start up..."
+sleep 15
+
+echo "Checking setup"
+npm test
+
+echo "Killing codeceptJS docs"
+ps aux | grep docusaurus-start | grep -v grep | awk '{print $2 }' | xargs kill
